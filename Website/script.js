30a81cf9232105a3344a97dd20f74fb5ed960e93
varsha0501 <khatvani.varsha@yahoo.com>
2023-06-15 21:58:09 +0530

    CopytoClipboard Code Updated

--- Website/script.js
+++ Website/script.js
@@ -1,14 +1,27 @@
+console.log('Hello! Best of Luck Varsha. You will be successful!');
+
+const copyBtn = document.querySelector("#copy-icon1");
+const img = document.querySelector("#image1");
+
+copyBtn.onclick = e => {
+    
+    // Copy Image to clipboard
+    const canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
+    canvas.toBlob((blob) => {
+      navigator.clipboard.write([
+          new ClipboardItem({ "image/png": blob })
+      ]);
+    }, "image/png");
+
+    // Convert image to text 
+    Tesseract.recognize(img).then(function(result){
+         console.log(result.text);
+         navigator.clipboard.writeText(result.text);
+    });
+
+};
 
 
-async function copyToClipboard() {
-   alert('Hello! Best of Luck Varsha. You will be successful!');
-
-   const response = await fetch('/Code-Examples/psudo-code.png');
-   
-   console.log(response.blob);
-
-   const blob = await response.blob;
-
-   await navigator.clipboard.write([new ClipboardItem({'image/png':blob})]);
-
-}
