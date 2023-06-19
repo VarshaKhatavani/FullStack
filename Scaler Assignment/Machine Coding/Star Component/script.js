15110bae25c6786c76a55c543e9eaceaa54286d5
varsha0501 <khatvani.varsha@yahoo.com>
2023-06-19 22:25:48 +0530

    Rating star - Solution

--- Scaler Assignment/Machine Coding/Star Component/script.js
+++ Scaler Assignment/Machine Coding/Star Component/script.js
@@ -1,32 +1,47 @@
 
 let starContainer = document.querySelector(".star-container");
 let allStars =  document.querySelectorAll(".star");
+let filled = 0;
+
+starContainer.addEventListener("click", function (e) {
+
+    let currClicked = e.target.dataset.index;
+    
+    for (let i = 0; i < 5; i++) {
+        allStars[i].classList.remove("star-filled");
+    }
+    
+    for (let i = 0; i < currClicked; i++) {
+        allStars[i].classList.add("star-filled");
+    }
+    
+    document.getElementById("count").innerText = `${currClicked}`;
+    
+    filled = currClicked;
+});
+    
 
 starContainer.addEventListener("mouseover", function(e){
 
     let currentSelected = e.target.dataset.index;
     console.log(currentSelected);
-    for(let i=0;i<currentSelected;i++){
-        allStars[i].classList.add("star-filled");
-        if(allStars[i].classList.contains("star-filled")){
-            allStars[i].classList.remove("star-filled")
-        }
+
+    for (let i = 0; i < 5; i++) {
+        allStars[i].classList.remove("star-filled");
     }
 
-    // for(let i=0;i<currentSelected;i++){
-    //     if(allStars[i].classList.contains("star-filled")){
-    //         allStars[i].classList.remove("star-filled")
-    //     }
-    // }
+    for(let i=0;i<currentSelected;i++){
+        allStars[i].classList.add("star-filled");
+    }
 });
 
 starContainer.addEventListener("mouseleave", function(e){
 
-    let currentSelected = e.target.dataset.index;
-    console.log(currentSelected);
-    for(let i=0;i<currentSelected;i++){
-        if(allStars[i].classList.contains("star-filled")){
-            allStars[i].classList.remove("star-filled")
-        }
+    for (let i = 0; i < 5; i++) {
+        allStars[i].classList.remove("star-filled");
+    }
+
+    for(let i=0;i<filled;i++){
+        allStars[i].classList.add("star-filled");
     }
 });
