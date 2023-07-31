79c1cecf2883f6aa893f207b04876311ef2c8979
varsha0501 <khatvani.varsha@yahoo.com>
2023-07-31 20:57:36 +0530

    React Router - Page Change without Refresh
    Install React Router DOM
    Browse Router with conditional Routing

--- React/IMDB-Project/imdb-app/src/App.js
+++ React/IMDB-Project/imdb-app/src/App.js
@@ -3,16 +3,24 @@ import Navbar from './Components/Navbar';
 import MovieList from './Components/MovieList';
 import Banner from './Components/Banner';
 import WatchList from './Components/WatchList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-   <>
-    <Navbar/>
-    <Banner/>
-    <MovieList/>
-    <WatchList/>
-   </>
-  );
+        <BrowserRouter>
+            <Navbar/> 
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Banner/>
+                  <MovieList/>           
+                </>
+              }></Route>
+              <Route path="/watchlist" element={<WatchList/>}></Route>
+              {/* Note - Path is specified in Navbar anchor-->Link tag */}
+            </Routes>
+        </BrowserRouter>  
+      );
 }
 
 export default App;
