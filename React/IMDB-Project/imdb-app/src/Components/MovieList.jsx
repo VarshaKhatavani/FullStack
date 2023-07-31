65075052454ea5d22f042c69d462408cad630646
varsha0501 <khatvani.varsha@yahoo.com>
2023-07-31 20:04:10 +0530

    Trending Movies Component Design

--- React/IMDB-Project/imdb-app/src/Components/MovieList.jsx
+++ React/IMDB-Project/imdb-app/src/Components/MovieList.jsx
@@ -1,11 +1,43 @@
 export default function MovieList(){
     return(
-        <div className="flex-auto">
+        <div className="flex-auto p-3">
             
-            <div>I am Trending Movie List</div>
-            <div>
-                <div>
-                    Barbie
+            <div className="text-3xl font-bold text-center p-3">I am Trending Movie List</div>
+            <div className="flex flex-wrap gap-4 justify-start">
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
+                </div>
+
+                <div className="h-[40vh] w-[180px] bg-cover rounded-2xl overflow-hidden hover:scale-110 duration-300 flex items-end" style={{backgroundImage:`url(https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg)`}} >    
+                    <div className="bg-stone-900/60 w-full p-2 text-center text-white"> John Wick</div>
                 </div>
             </div>
             
