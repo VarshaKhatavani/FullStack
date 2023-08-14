824f9210c560b31a039f2feafb1c0aef72c57606
varsha0501 <khatvani.varsha@yahoo.com>
2023-08-14 19:55:48 +0530

    Remove src extra folder

--- React/src/Components/Banner.jsx
+++
@@ -1,5 +0,0 @@
-export default function Banner(){
-    return(
-    <div>I am Banner</div>
-    )
-}
--- React/src/Components/WatchList.jsx
+++
@@ -1,5 +0,0 @@
-export default function WatchList(){
-    return(
-    <div>I am WatchList</div>
-    )
-}
