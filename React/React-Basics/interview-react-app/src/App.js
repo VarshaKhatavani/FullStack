95f12dddf3c12f4b89c8bc9640b0fbf0340529f2
varsha0501 <khatvani.varsha@yahoo.com>
2023-09-21 13:01:30 +0530

    useEffect usecases - Day 3

--- React/React-Basics/interview-react-app/src/App.js
+++ React/React-Basics/interview-react-app/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Ueffect from './components/ueffect';
+
 
 function App() {
   return (
+    <>
    <h1>Hello, Varsha! Welcome to first React App!</h1>
+   <Ueffect/>
+   </>
   );
 }
 
---
+++ React/React-Basics/interview-react-app/src/components/ueffect.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export default function Ueffect() {
+  // Initializing state variable 'count' with an initial value of 0
+  let [count, setCount] = useState(0);
+  let [text, setText] = useState("");
+
+  // Event handler function to increment 'count' when a button is clicked
+  let handleClick = () => {
+    setCount(count + 1);
+  };
+
+  let handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("inside useffect");
+    document.title = `value of count is ${count}`;
+  }, [count]);
+
+  return (
+    <>
+      <p>Hello, I am use effect component</p>
+      <div>
+        <h1> Count is : {count}</h1>
+        <button onClick={handleClick}>Increment Count</button>
+
+        <h2>Form</h2>
+        <input type="text" onChange={handleChange} value={text}></input>
+
+        <h3>{text}</h3>
+      </div>
+    </>
+  );
+}
