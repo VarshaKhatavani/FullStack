6699899094ff82b170cb69d48611450ac5662a1d
varsha0501 <khatvani.varsha@yahoo.com>
2023-09-21 20:39:29 +0530

    Render Class Dynamically

--- React/React-Basics/cart-react-app/src/components/counter.jsx
+++ React/React-Basics/cart-react-app/src/components/counter.jsx
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
   };
   render() {
     return (
       <div>
         <h1> Hello, I am counter component </h1>
-        <span style={{ fontSize: 18 }} className="badge bg-primary m-2">
+        <span
+          style={{ fontSize: 18 }}
+          className={this.getClassByCounterValue()}
+        >
           {this.formatCount()}
         </span>
         <button className="btn btn-light btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getClassByCounterValue() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count > 0 ? "primary" : "warning";
+    return classes;
   }
 
   formatCount() {
