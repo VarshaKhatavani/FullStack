951b1fbf9a70b79943b2f9bbbac36feb1d446757
varsha0501 <khatvani.varsha@yahoo.com>
2023-09-29 10:11:29 +0530

    Dynamically display no of pages to show

--- React/IMDB-Project/imdb-app/src/Components/MovieList.jsx
+++ React/IMDB-Project/imdb-app/src/Components/MovieList.jsx
@@ -3,42 +3,79 @@ import MovieCard from "./MovieCard";
 import Pagination from "./Pagination";
 import axios from "axios";
 
-export default function MovieList(){
-    
-    let [moviesObj, setMovieObj] = useState(undefined);    
+export default function MovieList() {
+  let [moviesObj, setMovieObj] = useState(undefined);
+  let [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 500;
 
-    useEffect(()=>{
-        axios.get("https://api.themoviedb.org/3/trending/movie/day?api_key=c4503da08fd450674fc73bc1114a40ee")
-        .then(function(response){
-            console.log(response.data);
-            //moviesObj = response.data ;
-            setMovieObj(response.data.results);
-        })      
-    },[])
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=c4503da08fd450674fc73bc1114a40ee&page=${currentPage}`
+      )
+      .then(function (response) {
+        console.log(response.data);
+        //moviesObj = response.data ;
+        setMovieObj(response.data.results);
+      });
+  }, []);
 
-    console.log(moviesObj);
-    
-    if(moviesObj === undefined ){
-        return(
-            <div>Loading...</div>
-        )
-    }    
-    
-    return(
-        <div className="flex-auto p-3">
-            
-            <div className="text-3xl font-bold text-center p-3">I am Trending Movie List</div>
-            
-            <div className="flex flex-wrap gap-4 justify-evenly">                
-                {
-                    moviesObj.map((mObject)=>{
-                        return <MovieCard title={mObject.title} poster_path={mObject.poster_path} />  
-                    })
-                }              
-            </div>  
+  console.log(moviesObj);
 
-            <Pagination/>      
-            
-        </div>        
-    )
+  if (moviesObj === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex-auto p-3">
+      <div className="text-3xl font-bold text-center p-3">
+        I am Trending Movie List
+      </div>
+
+      <div className="flex flex-wrap gap-4 justify-evenly">
+        {moviesObj.map((mObject) => {
+          return (
+            <MovieCard
+              key={mObject.id}
+              title={mObject.title}
+              poster_path={mObject.poster_path}
+            />
+          );
+        })}
+      </div>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChanges={handlePageChange}
+        calculatePagesToShow={calculatePagesToShow}
+      />
+    </div>
+  );
+
+  // For example, show 5 pages centered around the current page
+  function calculatePagesToShow() {
+    const pagesToShow = [];
+    const pagesToDisplay = 5;
+
+    // start page will be
+    let startPage = Math.max(currentPage - Math.floor(pagesToDisplay / 2), 1);
+    let endPage = startPage + pagesToDisplay - 1;
+
+    // handle edge case for last page
+    if (endPage > totalPages) {
+      endPage = totalPages;
+      startPage = Math.max(endPage - pagesToDisplay + 1, 1);
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      pagesToShow.push(i);
+    }
+
+    return pagesToShow;
+  }
+
+  function handlePageChange(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
 }
--- React/IMDB-Project/imdb-app/src/Components/Pagination.jsx
+++ React/IMDB-Project/imdb-app/src/Components/Pagination.jsx
@@ -1,9 +1,31 @@
-export default function Pagination(){
-    return(
-        <div className="flex justify-center m-4">
-            <div className="p-2 hover:cursor-pointer">Prev</div>
-            <div className="p-2 hover:cursor-pointer">1</div>
-            <div className="p-2 hover:cursor-pointer">Next</div>
-        </div>
-    )
+export default function Pagination(props) {
+  let current = props.currentPage;
+  console.log(current);
+
+  const pagesToShow = props.calculatePagesToShow();
+  console.log(pagesToShow);
+  const pageButtons = pagesToShow.map((page) => (
+    <span
+      key={page}
+      onClick={() => props.onPageChanges(page)}
+      className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+    >
+      {page}
+    </span>
+    // <button
+    //   key={page}
+    //   onClick={() => props.onPageChanges(page)}
+    //   className={page === props.currentPage ? "active" : ""}
+    // >
+    //   {page}
+    // </button>
+  ));
+
+  return (
+    <div className="flex justify-center m-4">
+      <div className="p-2 hover:cursor-pointer">Prev</div>
+      <span className="flex hover:cursor-pointer">{pageButtons}</span>
+      <div className="p-2 hover:cursor-pointer">Next</div>
+    </div>
+  );
 }
