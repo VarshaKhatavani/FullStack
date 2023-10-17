a481896d7a9c647b18df99ac2343fde45c006a8c
varsha0501 <khatvani.varsha@yahoo.com>
2023-10-17 16:49:27 +0530

    Create an element using React

--- React/Refresher/App.js
+++ React/Refresher/App.js
@@ -1,3 +1,29 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// <div id="parent">
+//     <div id="child">
+//         <h1>Hey, I'm h1 tag</h1>
+//         <h2>Hi, I'm h2 tag</h2>
+//     </div>
+//      <div id="child2">
+//         <h1>Hey, I'm h1 tag</h1>
+//         <h2>Hi, I'm h2 tag</h2>
+//     </div>
+// </div>
+
+
+const parent = React.createElement("div", {id:"parent"}, 
+    [React.createElement("div", {id:"child"}, [
+        React.createElement("h1",{},"Hey, I'm h1 tag"), 
+        React.createElement("h2",{},"Hi, I'm h2 tag")
+    ]),
+     React.createElement("div", {id:"child2"}, [
+        React.createElement("h1",{},"Hey, I'm h1 tag"), 
+        React.createElement("h2",{},"Hi, I'm h2 tag")]
+    )]
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
