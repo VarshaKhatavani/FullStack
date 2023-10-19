a085f406025d44aba065f1a9218ef27a2339f431
varsha0501 <khatvani.varsha@yahoo.com>
2023-10-19 22:42:13 +0530

    Normal Function and component call

--- React/Refresher/App.js
+++ React/Refresher/App.js
@@ -37,7 +37,9 @@ const jsxHeading = <h1>Hey, I'm h1 tag.</h1>
 // Use of functional component - type 1 - without using return 
 const HeadingComponent = () => (
         <div id="container">
-            <Title/>
+            {/* <Title/>  */}
+            {Title()}  {/* Component itself is a javaScipt function so it can be called as well like function calling */}
+            <Comp/> 
             <h1>Hi, I'm from heading component</h1>
         </div>
 )
@@ -45,6 +47,10 @@ const HeadingComponent = () => (
 // Use of functional component - type 2 - with using return 
 const Title = () =>{
     return  <h1 className="title"> Hi, I'm title component </h1>
+}
+
+const Comp = function(){
+    return <h1>Hi, I'm normal function</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
