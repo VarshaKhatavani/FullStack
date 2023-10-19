169ae983548909541074937318943ae721f171e9
varsha0501 <khatvani.varsha@yahoo.com>
2023-10-19 18:00:44 +0530

    Use of JSX and Scripts

--- React/Refresher/App.js
+++ React/Refresher/App.js
@@ -17,15 +17,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, 
-    [React.createElement("div", {id:"child"}, [
-        React.createElement("h1",{},"Hey, I'm h1 tag"), 
-        React.createElement("h2",{},"Hi, I'm h2 tag")
-    ]),
-     React.createElement("div", {id:"child2"}, [
-        React.createElement("h1",{},"Hey, I'm h1 tag"), 
-        React.createElement("h2",{},"Hi, I'm h2 tag")]
-    )]
-);
+// const parent = React.createElement("div", {id:"parent"}, 
+//     [React.createElement("div", {id:"child"}, [
+//         React.createElement("h1",{},"Hey, I'm h1 tag"), 
+//         React.createElement("h2",{},"Hi, I'm h2 tag")
+//     ]),
+//      React.createElement("div", {id:"child2"}, [
+//         React.createElement("h1",{},"Hey, I'm h1 tag"), 
+//         React.createElement("h2",{},"Hi, I'm h2 tag")]
+//     )]
+// );
+
+// Use of JSX
+const heading = <h1>Hey, I'm h1 tag.</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);   
