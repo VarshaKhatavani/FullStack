4d2849697456306802ef6bd6a29ab7b4d604a0b9
varsha0501 <khatvani.varsha@yahoo.com>
2023-10-20 00:12:16 +0530

    Inject JSX and JS Element

--- React/Refresher/App.js
+++ React/Refresher/App.js
@@ -39,18 +39,24 @@ const HeadingComponent = () => (
         <div id="container">
             {/* <Title/>  */}
             {Title()}  {/* Component itself is a javaScipt function so it can be called as well like function calling */}
+            <span>Total = </span> {100 + 100}
             <Comp/> 
             <h1>Hi, I'm from heading component</h1>
         </div>
 )
 
+const elem = <span>Hello, I'm span tag using JSX & JS to inject element</span>
+
 // Use of functional component - type 2 - with using return 
-const Title = () =>{
+const Title = () =>{    
     return  <h1 className="title"> Hi, I'm title component </h1>
 }
 
 const Comp = function(){
-    return <h1>Hi, I'm normal function</h1>
+    return <div>
+        <h1>Hi, I'm normal function</h1>  
+        {elem}
+    </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
