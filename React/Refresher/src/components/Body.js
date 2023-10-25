6f1618731b9751eda86570dcbf1201664f055a29
varsha0501 <khatvani.varsha@yahoo.com>
2023-10-25 13:17:09 +0530

    Episode 5 - Part 2 - filter by average rating

--- React/Refresher/src/components/Body.js
+++ React/Refresher/src/components/Body.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import {resList} from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Content = () =>{
+
+    let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    console.log(listOfRestaurants);
+
     return ( 
         <>           
-            <div className="search">search</div>        
+            <div className="filter">
+                <button onClick={()=>{ 
+                    const filteredlist = listOfRestaurants.filter(
+                        (res)=>res.data.avgRating > 4
+                    ); 
+                    console.log(filteredlist);
+                    setListOfRestaurants(filteredlist);
+                    
+                }} className="filter-btn">Top Rated Restaurants</button>
+            </div>        
             <div className="res-container">{
-                resList.map((restaurant)=>(
+                listOfRestaurants.map((restaurant)=>(
                     <RestaurantCard key={restaurant.data.id} resData={restaurant} />
                 ))
             }               
