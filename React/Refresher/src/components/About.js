0f5efbf534ef9c13b693e11faaa37bce41cd864e
varsha0501 <khatvani.varsha@yahoo.com>
2023-11-21 21:18:47 +0530

    Episode 8 - Update state variable in class based / life cycle
    - class component life cycle

--- React/Refresher/src/components/About.js
+++ React/Refresher/src/components/About.js
@@ -1,12 +1,35 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () =>{
-    return <div>
-      <h1>About Us </h1>  
-      <User name={"Varsha Khatavani (Functional component)"} location={"Pune"} />
-      <UserClass  name={"Varsha Khatavani (Class component)"} location={"Navsari"}/>
-    </div>
+// const About = () =>{
+//     return <div>
+//       <h1>About Us </h1>  
+//       <User name={"Varsha Khatavani (Functional component)"} location={"Pune"} />
+//       <UserClass  name={"Varsha Khatavani (Class component)"} location={"Navsari"}/>
+//     </div>
+// }
+
+class About extends Component{
+
+    constructor(props){
+        super(props);
+        console.log('Parent Constructor') ;
+    }
+
+    componentDidMount(){
+      console.log('Parent Component Did Mount');
+    }
+
+    render(){
+       console.log('Parent Render') ;
+        return(
+          <div>
+              <h1>About Us</h1>
+              <UserClass name={"Varsha Khatavani(Class Component)"} location={"Navsari"} />
+          </div>
+        )
+    }
 }
 
 export default About ;
--- React/Refresher/src/components/UserClass.js
+++ React/Refresher/src/components/UserClass.js
@@ -5,16 +5,20 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         console.log(props);
-
         // multiple state variables
         this.state = {
             count : 0 ,
             count2 : 2 ,
         }
-    }    
+        console.log('Child Constructor');
+    }  
+    
+    componentDidMount(){
+        console.log('Child DidMount');
+    }
 
     render(){
-    
+        console.log('Child Render');
     // destructuring 
      const {count, count2} = this.state;
      const {name, location} = this.props;
@@ -22,6 +26,13 @@ class UserClass extends React.Component{
       return  <div className="user-card">
             <h1>Count : {count}</h1>   {/* {this.state.count}*/}
             <h1>Count2 : {count2}</h1>
+            <button onClick={()=>{
+                this.setState({
+                    count:this.state.count + 1,
+                });
+            }}>
+            Increase Count
+            </button>
             <h2>Name : {name}</h2>     {/* {this.props.name}*/}
             <h3>Location : {location}</h3>
             <h4>Contact : varshakhatwani013@gmail.com</h4>
