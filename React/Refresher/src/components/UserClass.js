d8842e542a4540c3a435bc8785da3f412602cf25
varsha0501 <khatvani.varsha@yahoo.com>
2023-11-22 09:50:58 +0530

    Episode 8 - API call in Class Based Component

--- React/Refresher/src/components/UserClass.js
+++ React/Refresher/src/components/UserClass.js
@@ -4,41 +4,30 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props);
-        console.log(props);
-        // multiple state variables
-        this.state = {
-            count : 0 ,
-            count2 : 2 ,
-        }
+        console.log(props);        
         console.log('Child Constructor');
+
+        this.state = {};
     }  
     
-    componentDidMount(){
+    async componentDidMount(){
         console.log('Child DidMount');
+        const data = await fetch("https://api.github.com/users/varsha0501");
+        const json = await data.json();
+        console.log(json);        
     }
 
     render(){
         console.log('Child Render');
-    // destructuring 
-     const {count, count2} = this.state;
-     const {name, location} = this.props;
+        // destructuring 
+        const {name, location} = this.props;
 
-      return  <div className="user-card">
-            <h1>Count : {count}</h1>   {/* {this.state.count}*/}
-            <h1>Count2 : {count2}</h1>
-            <button onClick={()=>{
-                this.setState({
-                    count:this.state.count + 1,
-                });
-            }}>
-            Increase Count
-            </button>
+        return  <div className="user-card">            
             <h2>Name : {name}</h2>     {/* {this.props.name}*/}
             <h3>Location : {location}</h3>
             <h4>Contact : varshakhatwani013@gmail.com</h4>
         </div>
     }
-
 }
 
 export  default UserClass ;
