0b76263534172cde344bd11bb4cf2ce7ab3bffb7
varsha0501 <khatvani.varsha@yahoo.com>
2023-11-25 16:03:51 +0530

    Episode 08 - Complete
    - update state variables using dynamic API values

--- React/Refresher/src/components/UserClass.js
+++ React/Refresher/src/components/UserClass.js
@@ -7,26 +7,37 @@ class UserClass extends React.Component{
         console.log(props);        
         console.log('Child Constructor');
 
-        this.state = {};
+        this.state = {
+            userInfo:{
+                name : "Dummy",
+                location : "Default",
+            }
+        };
     }  
     
     async componentDidMount(){
         console.log('Child DidMount');
         const data = await fetch("https://api.github.com/users/varsha0501");
         const json = await data.json();
+
+        this.setState({
+            userInfo:json,
+        })
+
         console.log(json);        
     }
 
     render(){
         console.log('Child Render');
         // destructuring 
-        const {name, location} = this.props;
+        const {name, location, avatar_url} = this.state.userInfo ;  // this.props;
 
-        return  <div className="user-card">            
-            <h2>Name : {name}</h2>     {/* {this.props.name}*/}
-            <h3>Location : {location}</h3>
-            <h4>Contact : varshakhatwani013@gmail.com</h4>
-        </div>
+        return  <div className="user-card"> 
+                    <img src={avatar_url}/>
+                    <h2>Name : {name}</h2>     {/* {this.props.name}*/}
+                    <h3>Location : {location}</h3>
+                    <h4>Contact : varshakhatwani013@gmail.com</h4>
+                </div>
     }
 }
 
