3b08a2e8f71bfe94413b271f7b06d71949ca7485
varsha0501 <khatvani.varsha@yahoo.com>
2024-01-07 23:02:11 +0530

    Class 2 - Use of Different Types of Middleware

--- Node/Node BookMyShow/server2.js
+++ Node/Node BookMyShow/server2.js
@@ -2,6 +2,44 @@ const express = require("express");
 
 const app = express();
 
+// application level middleware
+app.use((req, res, next)=>{
+    console.log(req.method, req.ip, req.hostname, new Date());
+    next(); // will allow next request to execute
+})
+
+// auth middleware
+const auth = (req, res, next)=>{
+    console.log(req.query); 
+    //  add in query string : http://localhost:8088/?password=123
+    // output : { password: '123' }
+    let password = req.query.password;
+    if(password==123){
+        next();
+    }
+    else{
+        res.send("You are not authorized person");
+    }
+}
+//app.use(auth);
+
+// route level middleware
+// query string : http://localhost:8088/users/?password=123
+app.get("/users", auth, (req,res)=>{
+    res.send({"type":"GET"});
+})
+
+
+// user specific middleware
+// query string : http://localhost:8088/users/123/?password=123
+app.get("/users/:id", auth, (req,res)=>{
+    console.log(req.params);
+    res.send({"type":"GET"});
+})
+
+// Built in middleware
+app.use(express.static('public'));
+
 app.get("/",(req,res)=>{
     res.send({"type":"GET"});
 })
