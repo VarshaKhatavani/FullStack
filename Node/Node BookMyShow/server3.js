b8a48db9f54332994bf450461fbb4fe22015fc11
varsha0501 <khatvani.varsha@yahoo.com>
2024-01-13 22:51:57 +0530

    Class 4 - Added Routes

--- Node/Node BookMyShow/server3.js
+++ Node/Node BookMyShow/server3.js
@@ -1,31 +1,12 @@
 const express = require("express");
-const prdtController = require("./Controllers/productController");
+const productRoute = require("./routes/productRoute");
+
 const app = express();
 const PORT = 8088;
 
-//get all products
-app.get("/products",prdtController.getAllProducts);
-// get specific product
-app.get("/products/:id",prdtController.getProductById);
-// post data to json
-//http://localhost:8088/products/
-app.post("/products",prdtController.createPost);
-// delete specific product
-app.delete("/products/:id",prdtController.deletePost);
-// put - major updates | add key/value pairs
-app.put("/products/:id",prdtController.replaceProduct);
-
-// http://localhost:8088/products/1
-//{
-//     "id":1,
-//     "title":"Laptop",
-//     "price":3400.00,
-//     "ratings":4.9
-// }
-// patch - minor updates | update json values
-app.patch("/products/:id",prdtController.updateProduct);
+app.use(express.json());
+app.use('/', productRoute.router);  // middleware is initilizing our router for us.
 
 app.listen(PORT, ()=>{
     console.log(`server is listening at ${PORT}`);
 })
-
