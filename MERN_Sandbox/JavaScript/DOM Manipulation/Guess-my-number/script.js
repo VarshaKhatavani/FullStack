43ddc01b7333f988b4cab21a3a40940b2f59e737
varsha0501 <khatvani.varsha@yahoo.com>
2024-03-05 18:38:33 +0530

    Code Refactor

--- MERN_Sandbox/JavaScript/DOM Manipulation/Guess-my-number/script.js
+++ MERN_Sandbox/JavaScript/DOM Manipulation/Guess-my-number/script.js
@@ -10,14 +10,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const inputNumber = Number(document.querySelector('.guess').value);
   console.log(inputNumber, typeof inputNumber);
 
   if (!inputNumber) {
-    document.querySelector('.message').textContent = '⚠️ Enter Number!';
+    displayMessage('⚠️ Enter Number!');
   } else if (inputNumber === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#00c04b';
     document.querySelector('.number').style.width = '30rem';
@@ -25,20 +29,17 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (inputNumber > secretNumber) {
+  } else if (inputNumber !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
+      displayMessage(
+        inputNumber > secretNumber ? '📈 Too High!' : '📉 Too Low!'
+      );
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'Opps!😓 You lost the game!';
+      displayMessage('Opps!😓 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
-  } else if (inputNumber < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too Low!';
-    score--;
-    document.querySelector('.score').textContent = score;
   }
 });
 
@@ -46,7 +47,7 @@ document.querySelector('.again').addEventListener('click', function () {
   console.log('again called');
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
