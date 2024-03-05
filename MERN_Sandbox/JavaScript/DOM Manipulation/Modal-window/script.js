21b7891090bfd92597a3e1550cd1b386f70126d6
varsha0501 <khatvani.varsha@yahoo.com>
2024-03-05 21:13:24 +0530

    Code Refactor - Added Keyboard Events

--- MERN_Sandbox/JavaScript/DOM Manipulation/Modal-window/script.js
+++ MERN_Sandbox/JavaScript/DOM Manipulation/Modal-window/script.js
@@ -5,20 +5,33 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// we need it on click so do not call like closeModal() / openModal()
+// it will call the function as soon as statement executed
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log(btnOpenModal[i].textContent);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+// on escape key button modal should close
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
 });
