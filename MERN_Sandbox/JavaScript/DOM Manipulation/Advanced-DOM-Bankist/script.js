867ab5d2c705047ab1164504a32f410f0a5a2a47
varsha0501 <khatvani.varsha@yahoo.com>
2024-03-09 15:54:50 +0530

    JS - Day 7 - DOM Traversing - Functionality - Dynamic Tab

--- MERN_Sandbox/JavaScript/DOM Manipulation/Advanced-DOM-Bankist/script.js
+++ MERN_Sandbox/JavaScript/DOM Manipulation/Advanced-DOM-Bankist/script.js
@@ -41,3 +41,62 @@ btnScrollTo.addEventListener('click', function () {
     behavior: 'smooth',
   });
 });
+
+// Page Navigation
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  //console.log(e);
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
+/**
+ *  <div class="operations__tab-container">
+          <button
+            class="btn operations__tab operations__tab--1 operations__tab--active"
+            data-tab="1"
+          >
+            <span>01</span>Instant Transfers
+          </button>
+          <button class="btn operations__tab operations__tab--2" data-tab="2">
+            <span>02</span>Instant Loans
+          </button>
+          <button class="btn operations__tab operations__tab--3" data-tab="3">
+            <span>03</span>Instant Closing
+          </button>
+        </div>
+ */
+
+//  Dynamic tab
+const tabButtons = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContent = document.querySelectorAll('.operations__content');
+
+//tabButtons.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+tabContainer.addEventListener('click', function (e) {
+  // const clicked = e.target.; // didn't work
+  // const clicked = e.target.parentElement; // didn't work
+
+  // find the closest element that matches operations__tab
+  const clicked = e.target.closest('.operations__tab');
+  // console.log(clicked);
+  let clickedId = clicked.dataset.tab;
+
+  if (!clicked) return;
+
+  //remove active button/ content class from buttons
+  tabButtons.forEach(t => t.classList.remove('operations__tab--active'));
+  tabContent.forEach(tc => tc.classList.remove('operations__content--active'));
+
+  // add class to clicked button
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clickedId}`)
+    .classList.add('operations__content--active');
+});
