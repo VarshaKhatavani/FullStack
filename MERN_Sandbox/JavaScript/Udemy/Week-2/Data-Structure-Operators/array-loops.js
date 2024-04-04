8cb42175c09b7991f93481bd323b163871fc771b
varsha0501 <khatvani.varsha@yahoo.com>
2024-04-04 23:31:13 +0530

    use of Map, Set
    - Convert Object to Map
    - Convert Map to Array
    - Destructure Object Entries

--- MERN_Sandbox/JavaScript/Udemy/Week-2/Data-Structure-Operators/array-loops.js
+++ MERN_Sandbox/JavaScript/Udemy/Week-2/Data-Structure-Operators/array-loops.js
@@ -23,3 +23,39 @@ for (const [i, el] of menu.entries()) {
 // 2: Bruschetta
 // 3: Garlic Bread
 // 4: Caprese Salad and so on...
+
+//Arrays
+const users = [
+  {
+    name: 'Varsha',
+    email: 'hello@gmail.com',
+  },
+];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+// get keys from object
+const properties = Object.keys(restaurant.openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days :`;
+console.log(openStr);
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+openStr.slice(-1);
+console.log(openStr);
+
+// get value from object
+const propertiesValue = Object.values(restaurant.openingHours);
+console.log(propertiesValue);
+
+// get value from object
+const entries = Object.entries(restaurant.openingHours);
+console.log(entries);
+
+//Destructure Entries
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we are open at ${open} and close at ${close}`);
+}
