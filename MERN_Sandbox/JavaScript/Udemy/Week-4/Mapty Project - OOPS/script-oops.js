eb889c5db186d2a0aaebe3f64895138152033953
varsha0501 <khatvani.varsha@yahoo.com>
2024-04-19 20:25:07 +0530

    #functionality : _loadMap, _showForm

--- MERN_Sandbox/JavaScript/Udemy/Week-4/Mapty Project - OOPS/script-oops.js
+++ MERN_Sandbox/JavaScript/Udemy/Week-4/Mapty Project - OOPS/script-oops.js
@@ -12,15 +12,58 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
-  constructor() {}
+  #map;
+  #mapEvent;
 
-  _getPosition() {}
+  constructor() {
+    this._getPosition();
+  }
 
-  _loadMap() {}
+  _getPosition() {
+    if (navigator.geolocation) {
+      console.log(navigator.geolocation);
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('could not get position');
+        }
+      );
+    }
+  }
 
-  _showForm() {}
+  _loadMap(position) {
+    console.log(position);
+    console.log(this);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude); // 21.2041728 72.8301568
+    console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu
+`);
+
+    this.#map = L.map('map', {
+      center: [latitude, longitude],
+      zoom: 13,
+    }); // .setView([latitude, longitude], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+      foo: 'bar',
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    console.log(this);
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
   _toggleElevationField() {}
 
   _newWorkout() {}
 }
+
+const app = new App();
