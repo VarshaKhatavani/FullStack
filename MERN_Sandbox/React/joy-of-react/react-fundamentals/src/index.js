e36ef99e18465b798ea5e11bd6435c08b9acb7dd
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-03 00:55:03 +0530

    Use of Render and Props

--- MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import ShoppingList from "./01-Render-ShoppingList";
+import FriendlyGreeting from "./02-Props-FriendlyGreeting";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const element = React.createElement("h1", { id: "hello" }, "Welcome to React!");
+// const element = React.createElement("h1", { id: "hello" }, "Welcome to React!");
 
-root.render(element);
+root.render(
+  <>
+    <ShoppingList />
+    <FriendlyGreeting name="Varsha" />
+    <FriendlyGreeting name="Julie" />
+  </>
+);
