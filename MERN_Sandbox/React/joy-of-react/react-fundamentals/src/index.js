9a6377c54e8fe080298537c58e7a8089e75e5667
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-02 19:44:29 +0530

    Use of React createElement

--- MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const element = React.createElement("h1", { id: "hello" }, "Welcome to React!");
 
-root.render(<App />);
+root.render(element);
