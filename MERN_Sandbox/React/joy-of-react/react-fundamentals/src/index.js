2ed7d3cef20d93e67b6e3fb369283131f0b934c0
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-03 14:22:00 +0530

    React JS - Day 2 - Use of Render Contact Card, Message, Children Props | 03-05-2024

--- MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ShoppingList from "./01-Render-ShoppingList";
-import FriendlyGreeting from "./02-Props-FriendlyGreeting";
+// import ShoppingList from "./01-render-shoppingList";
+// import FriendlyGreeting from "./02-props-friendlyGreeting";
+// import MessageElement from "./03-render-message";
+import ContactCard from "./04-component-contactCard";
+import Button from "./05-children-prop-button";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // const element = React.createElement("h1", { id: "hello" }, "Welcome to React!");
 
 root.render(
   <>
-    <ShoppingList />
+    {/* <ShoppingList />
     <FriendlyGreeting name="Varsha" />
     <FriendlyGreeting name="Julie" />
+    <MessageElement /> */}
+    <ContactCard
+      name="Sunita Kumar"
+      job="Electrical Engineer"
+      email="sunita.kumar@acme.co"
+    />
+    <ContactCard
+      name="Henderson G. Sterling II"
+      job="Receptionist"
+      email="henderson-the-second@acme.co"
+    />
+    <Button status="Cancel">Cancel</Button>
+    <Button status="Confirm">Confirm</Button>
   </>
 );
