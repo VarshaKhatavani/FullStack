a950309e9ef1b0e8ed06926a5187b204b1c770ed
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-03 14:22:00 +0530

    React JS - Day 2 - Use of Render Contact Card, Message, Children Props | 03-05-2024

---
+++ MERN_Sandbox/React/joy-of-react/npm-packages-used.md
@@ -0,0 +1,3 @@
+<h3> Installed Packages </h3>
+
+<li>npm date-fns :  ls node_modules/date-fns </li>
---
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/03-date-helper.jsx
@@ -0,0 +1,10 @@
+import { format } from "date-fns";
+/*
+  This convenient helper function takes an
+  ISO date string, and formats it like:
+  
+  March 14th, 2024 at 11:09 AM
+*/
+export function formatDate(date) {
+  return format(new Date(date), "MMMM do 'at' hh:mm b");
+}
