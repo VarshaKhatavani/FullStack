dc73ce2866351c6f4fe60b39b9aa2b7a03d9bcc4
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-07 15:17:35 +0530

    Dynamically add style to component

--- MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import ShoppingList from "./01-render-shoppingList";
-// import FriendlyGreeting from "./02-props-friendlyGreeting";
-// import MessageElement from "./03-render-message";
-import ContactCard from "./2. props/04-component-contactCard";
-import Button from "./2. props/05-children-prop-button";
+import ProfileCardApp from "./4. styling/09-profileCard-app";
+// import ShoppingCart from "./3. map/08-shoppingCart";
+// import ShoppingList from "./1. render/01-render-shoppingList";
+// import FriendlyGreeting from "./2. props/02-props-friendlyGreeting";
+// import MessageElement from "./1. render/03-render-message";
+// import ContactCard from "./2. props/04-component-contactCard";
+// import Button from "./2. props/05-children-prop-button";
+// import MapContactCard from "./3. map/06-map-contactCard";
+// import AvatarApp from "./3. map/07-map-avatar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // const element = React.createElement("h1", { id: "hello" }, "Welcome to React!");
@@ -15,7 +19,7 @@ root.render(
     {/* <ShoppingList />
     <FriendlyGreeting name="Varsha" />
     <FriendlyGreeting name="Julie" />
-    <MessageElement /> */}
+    <MessageElement />
     <ContactCard
       name="Sunita Kumar"
       job="Electrical Engineer"
@@ -28,5 +32,9 @@ root.render(
     />
     <Button status="Cancel">Cancel</Button>
     <Button status="Confirm">Confirm</Button>
+    <MapContactCard />
+    <AvatarApp />
+    <ShoppingCart /> */}
+    <ProfileCardApp />
   </>
 );
