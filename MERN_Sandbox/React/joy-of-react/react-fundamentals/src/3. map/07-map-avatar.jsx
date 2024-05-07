46c232e8cff76e98fb40ce056ad9555e0cd65b8d
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-07 14:55:11 +0530

    React JS - Day 03 - Use of map to render list of data | 07-05-2024

--- MERN_Sandbox/React/joy-of-react/react-fundamentals/src/3. map/07-map-avatar.jsx
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/3. map/07-map-avatar.jsx
@@ -1,27 +1,38 @@
 import React from "react";
-import "./07-avatar.css";
+import "../3. map/07-avatar.css";
+import Avatar from "./07-Avatar";
 
-const Avatar = () => {
+const data = [
+  {
+    id: "001",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/001.png",
+    alt: "person with curly hair and a black T-shirt",
+  },
+  {
+    id: "002",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/002.png",
+    alt: "person wearing a hijab and glasses",
+  },
+  {
+    id: "003",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/003.png",
+    alt: "person with short hair wearing a blue hoodie",
+  },
+  {
+    id: "004",
+    src: "https://sandpack-bundler.vercel.app/img/avatars/004.png",
+    alt: "person with a pink mohawk and a raised eyebrow",
+  },
+];
+
+const AvatarApp = () => {
   return (
     <div className="avatar-set">
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/001.png"
-        alt="person with curly hair and a black T-shirt"
-      />
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/002.png"
-        alt="person wearing a hijab and glasses"
-      />
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/003.png"
-        alt="person with short hair wearing a blue hoodie"
-      />
-      <Avatar
-        src="https://sandpack-bundler.vercel.app/img/avatars/004.png"
-        alt="person with a pink mohawk and a raised eyebrow"
-      />
+      {data.map((profile) => {
+        return <Avatar id={profile.id} src={profile.src} alt={profile.alt} />;
+      })}
     </div>
   );
 };
 
-export default Avatar;
+export default AvatarApp;
