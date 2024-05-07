070d53370917f1e62c593a451221212fd3750f77
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-07 16:27:30 +0530

    Interview Specific - Graph & Grid

--- MERN_Sandbox/React/joy-of-react/react-fundamentals/src/utils/range.jsx
+++ MERN_Sandbox/React/joy-of-react/react-fundamentals/src/utils/range.jsx
@@ -4,13 +4,13 @@ export const range = (start, end, step = 1) => {
   let output = [];
 
   if (typeof end === "undefined") {
-    end = start;
-    start = 0;
+    end = start; // 4
+    start = 0; // 0
   }
 
   for (let i = start; i < end; i += step) {
     output.push(i);
   }
-
+  console.log(output);
   return output;
 };
