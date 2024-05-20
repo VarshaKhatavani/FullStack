9bbf0d31b07761278636e1f0c062e971dcde5ff0
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-20 22:38:21 +0530

    ## Exercise 2: Keeping track of guesses

--- MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Game/Game.js
+++ MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Game/Game.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+import GuessInput from "../GuessInput/GuessInput";
+import GuessResult from "../GuessResult/GuessResult";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,32 +11,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  const [inputWord, setInputWord] = React.useState("");
+  const [inputGuessList, setInputGuessList] = React.useState([]);
 
-  const handleInput = (e) => {
-    e.preventDefault();
-    console.log(inputWord);
+  const handleSubmitGuess = (inputWord) => {
+    setInputGuessList([...inputGuessList, inputWord]);
   };
 
   return (
     <>
-      <form className="guess-input-wrapper" onSubmit={handleInput}>
-        <label htmlFor="guess-input">Enter guess:</label>
-        <input
-          required
-          minLength={5}
-          maxLength={5}
-          pattern="[a-zA-Z]{5}"
-          title="5 letter word"
-          id="guess-input"
-          type="text"
-          value={inputWord}
-          onChange={(e) => {
-            let userInput = e.target.value;
-            setInputWord(userInput.toUpperCase());
-          }}
-        />
-      </form>
+      {/* Display Result */}
+      <GuessResult guessList={inputGuessList} />
+      {/* Input Form mentioned in this component  */}
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
 }
