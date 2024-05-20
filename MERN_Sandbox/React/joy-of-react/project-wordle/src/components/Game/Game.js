064b144ede233547cdc000c75af06c8c23a332ac
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-20 20:07:52 +0530

    ## Exercise 1: GuessInput

--- MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Game/Game.js
+++ MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,34 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [inputWord, setInputWord] = React.useState("");
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    console.log(inputWord);
+  };
+
+  return (
+    <>
+      <form className="guess-input-wrapper" onSubmit={handleInput}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          required
+          minLength={5}
+          maxLength={5}
+          pattern="[a-zA-Z]{5}"
+          title="5 letter word"
+          id="guess-input"
+          type="text"
+          value={inputWord}
+          onChange={(e) => {
+            let userInput = e.target.value;
+            setInputWord(userInput.toUpperCase());
+          }}
+        />
+      </form>
+    </>
+  );
 }
 
 export default Game;
