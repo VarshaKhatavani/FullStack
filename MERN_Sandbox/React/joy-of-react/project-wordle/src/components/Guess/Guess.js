d7726aecd43b730647c41aadfc3d3545ce56ad28
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-21 23:10:02 +0530

    React JS - Day 9 - #checkGuess functionality - to check letters misplaced, incorrect, correct positions | 21-05-2024

--- MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Guess/Guess.js
+++ MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Guess/Guess.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value = "" }) {
+function Guess({ value, answer }) {
+  let res = checkGuess(value, answer);
+  console.log(res);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
         <span key={num} className="cell">
+          {" "}
+          {/** className : cell misplaced, cell incorrect,cell correct */}
           {value ? value[num] : undefined}
         </span>
       ))}
