fbfe7b28802676473d058532b067e87a473691ad
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-28 13:58:18 +0530

    #Step 4: Implemented Game logic
    Generated array of object to Check the status of spelling

--- MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Guess/Guess.js
+++ MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Guess/Guess.js
@@ -3,18 +3,32 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+// add Letters with class name
+function GenerateStyledCell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
-  let res = checkGuess(value, answer);
-  console.log(res);
+  let result = checkGuess(value, answer);
+  console.log("result");
+  console.log(result);
 
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {" "}
-          {/** className : cell misplaced, cell incorrect,cell correct */}
-          {value ? value[num] : undefined}
-        </span>
+        <GenerateStyledCell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
+
+        // <span key={num} className="cell">
+        //   {" "}
+        //   {/** className : cell misplaced, cell incorrect,cell correct */}
+        //   {value ? value[num] : undefined}
+        // </span>
       ))}
     </p>
   );
