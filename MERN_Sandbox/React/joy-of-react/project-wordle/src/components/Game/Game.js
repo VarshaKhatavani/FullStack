929d4224738f67f482dad1b07044848de66d5b30
varsha0501 <khatvani.varsha@yahoo.com>
2024-05-29 13:52:27 +0530

    #step 5 : Win Lose Banner | 29-05-2024

--- MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Game/Game.js
+++ MERN_Sandbox/React/joy-of-react/project-wordle/src/components/Game/Game.js
@@ -1,9 +1,11 @@
 import React from "react";
-
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResult from "../GuessResult/GuessResult";
+import WonBanner from "../WonBanner/WonBanner";
+import LostBanner from "../LostBanner/LostBanner";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,18 +13,34 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  // running | won | lost
+  const [gameStatus, setGameStatus] = React.useState("running");
   const [inputGuessList, setInputGuessList] = React.useState([]);
 
   const handleSubmitGuess = (inputWord) => {
-    setInputGuessList([...inputGuessList, inputWord]);
+    const nextGuesses = [...inputGuessList, inputWord];
+    setInputGuessList(nextGuesses);
+
+    console.log("nextGuesses....", nextGuesses);
+
+    if (inputWord === answer) {
+      setGameStatus("won");
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost");
+    }
   };
 
   return (
     <>
       {/* Display Result */}
+      {gameStatus}
       <GuessResult guessList={inputGuessList} answer={answer} />
       {/* Input Form mentioned in this component  */}
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
+      {gameStatus === "won" && (
+        <WonBanner noOfGuesses={inputGuessList.length} />
+      )}
+      {gameStatus === "lost" && <LostBanner answer={answer} />}
     </>
   );
 }
