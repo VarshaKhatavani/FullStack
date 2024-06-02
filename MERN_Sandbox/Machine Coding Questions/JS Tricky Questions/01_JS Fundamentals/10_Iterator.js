1d4c100b1ab7128e868693b1e082ae2f98882231
varsha0501 <khatvani.varsha@yahoo.com>
2024-06-02 15:49:32 +0530

    JS Interview - 5/30 {Iterator, Generator, tryCatch} | 02-06-2024

--- MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/01_JS Fundamentals/10_Iterator.js
+++ MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/01_JS Fundamentals/10_Iterator.js
@@ -3,3 +3,38 @@
  * Q.2) How do you create an iterator ?
  * Q.3) Explain a practical use of an iterator ?
  */
+
+let arr = [1, 2, 3, 4];
+for (let i of arr) console.log(i);
+
+let array = [5, 6, 7, 8];
+let itr = array[Symbol.iterator]();
+console.log(itr); // {}
+console.log(itr.next()); // { value: 5, done: false }
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next()); // { value: undefined, done: true }
+
+// Code 3
+let obj = {
+  a: 10,
+  b: 20,
+  [Symbol.iterator]() {
+    return {
+      next: function () {
+        if (obj.a < obj.b) {
+          return {
+            value: obj.a++,
+            done: false,
+          };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  },
+};
+for (let i of obj) {
+  console.log(i);
+}
--- MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/01_JS Fundamentals/12_tryCatch.js
+++ MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/01_JS Fundamentals/12_tryCatch.js
@@ -4,3 +4,55 @@ Q.3) Can you generate SyntaxError or ReferenceError kind of errors ?
 Q.4) What is the purpose of the finally block ?
 Q.5) How can you refer to the name or description of error ?
 Q.6) Can we have finally without a catch block as well ? */
+
+// let a = 10;
+// console.log(`value of ${a}`); // value of 10
+
+// letb = 20;
+// console.log(`value of ${b}`); // ReferenceError: b is not defined
+
+//Code 2
+try {
+  let a = 10;
+  console.log(`value of ${a}`); // value of 10
+
+  letb = 20;
+  console.log(`value of ${b}`); // ReferenceError: b is not defined
+} catch (error) {
+  console.log("Error Found");
+}
+
+//Code 3
+try {
+  let a = 10;
+  console.log(`value of ${a}`); // value of 10
+  letb = 20;
+  console.log(`value of ${b}`); // ReferenceError: b is not defined
+} catch (error) {
+  console.log("Error Found");
+  console.log("Error Name : ", error.name);
+  console.log("Error Message :", error.message);
+  console.log("Error Stack :", error.stack);
+}
+
+// Code 4 - Custom Error
+let a = 8;
+try {
+  if (a < 18) throw new Error("Age should be greater than 18");
+  else console.log("Valid age");
+} catch (error) {
+  console.log(error.name);
+  console.log("Invalid age error : ", error.message);
+}
+
+// Code 5 - type Error - helps in debugging type-related issues.
+let no = "10";
+try {
+  if (no < 100) throw new TypeError("Invalid age!");
+  else console.log("Valid age!");
+} catch (err) {
+  console.log(err.name); //TypeError;
+  console.log(err.message); //Invalid age!
+} finally {
+  console.log("Alway Run Finally code");
+}
