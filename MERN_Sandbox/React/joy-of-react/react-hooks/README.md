274e5fa31920f3478dc366027c60088a86a038b8
varsha0501 <khatvani.varsha@yahoo.com>
2024-06-06 23:59:29 +0530

    #React useRef hooks | 06-06-2024

--- MERN_Sandbox/React/joy-of-react/react-hooks/README.md
+++ MERN_Sandbox/React/joy-of-react/react-hooks/README.md
@@ -1,3 +1,5 @@
+<h1> React.useId() hook </h1>
+
 ## Login Form - Used in header and footer but react adds unique id using React.useId()
 
 ![alt text](image-2.png)
@@ -22,3 +24,24 @@
 ![alt text](image-4.png)
 
 ![alt text](image-5.png)
+
+<h1> React.useRef() hook </h1>
+
+<p>React.useRef() hook will contain the current properties of selected element. </p>
+## use of window.stateValues
+
+![alt text](image-6.png)
+
+## Canvas App
+
+![alt text](image-7.png)
+
+## Video Player
+
+<video controls src="src/02_useRef-hook/03_videoplayer/VideoPlayer_Initial.mp4"></video>
+
+<video controls src="src/02_useRef-hook/03_videoplayer/VideoPlayer_After_Speed.mp4"></video>
+
+## Media Player
+
+![alt text](image-8.png)
--- MERN_Sandbox/React/joy-of-react/react-hooks/src/App.js
+++ MERN_Sandbox/React/joy-of-react/react-hooks/src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 // import ToggleModeApp from "./01_useId-hook/02_dark-mode/ToggleModeApp";
 // import TextInputApp from "./01_useId-hook/03_inputText-email/App";
 // import SearchApp from "./01_useId-hook/04_search-useId-hook/SearchApp";
-import LoginApp from "./01_useId-hook/05_loginForm/LoginApp";
+// import LoginApp from "./01_useId-hook/05_loginForm/LoginApp";
+
+// import NameApp from "./02_useRef-hook/01_retain-stateValues/NameApp";
+// import CanvasApp from "./02_useRef-hook/02_render-canvas/CanvasApp";
+// import VideoPlayerApp from "./02_useRef-hook/03_videoplayer/VideoPlayerApp";
+import MediaPlayerApp from "./02_useRef-hook/04_mediaplayer/MediaPlayerApp";
 
 function App() {
   return (
@@ -13,7 +18,11 @@ function App() {
       {/* <ToggleModeApp /> */}
       {/* <TextInputApp /> */}
       {/* <SearchApp /> */}
-      <LoginApp />
+      {/* <LoginApp /> */}
+      {/* <NameApp /> */}
+      {/* <CanvasApp /> */}
+      {/* <VideoPlayerApp /> */}
+      <MediaPlayerApp />
     </>
   );
 }
