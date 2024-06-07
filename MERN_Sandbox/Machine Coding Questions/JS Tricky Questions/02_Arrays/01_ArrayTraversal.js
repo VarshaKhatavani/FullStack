9a6e5e63dd07b1ee9a8024e2c1bc6a1a716f0130
varsha0501 <khatvani.varsha@yahoo.com>
2024-06-07 14:29:55 +0530

    #Array Applications { push(), pop(), shift(), unshift(), splice() } | 07-06-2024

--- MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/01_ArrayTraversal.js
+++ MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/01_ArrayTraversal.js
@@ -1,3 +1,38 @@
 /**Q.1) What is the difference between for...in and for...of ?
 Q.2) What will be the output of the below code ?
 Q.3) What will be the output of below statements ? */
+
+// Code 1
+let arr = [1, 2, 3, 4, 5];
+let arr1 = ["Hello", 16, true, 3.3];
+console.log(arr);
+console.log(arr1);
+//access specific element
+console.log(arr1[0]); // Hello
+console.log(arr1.length); // 4
+console.log(arr[arr.length - 1]); // 5
+
+// Code 2
+let arrProduct = [
+  { pCode: 101, pName: "iPhone 15 plus" },
+  { pCode: 102, pName: "SAMSUNG galaxy J8" },
+  { pCode: 103, pName: "MAX" },
+];
+console.log(arrProduct);
+console.log(arrProduct[0].pName); // iPhone 15 plus
+
+// Code 3
+let JSVersion = ["ES6", "ES7", "ES8", "ES9"];
+for (let i = 0; i < JSVersion.length; i++) {
+  console.log(JSVersion[i]);
+}
+
+// for in loop - represent index
+for (let elem in JSVersion) {
+  console.log(elem); // 0 1 2 3
+}
+
+// for of loop
+for (let elem of JSVersion) {
+  console.log(elem); // ES6 ES7 ES8 ES9
+}
--- MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/02_ArrayApplications.js
+++ MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/02_ArrayApplications.js
@@ -12,3 +12,49 @@ Q.6)  If there is not element removed then what will splice() method return ?
 */
 
 // array.push() method returns the count of array
+
+// Code 1 : push()
+let arr = ["one", "two", "three", "four", "five"];
+console.log(arr.push("six")); // 6
+console.log(arr); // [ 'one', 'two', 'three', 'four', 'five', 'six' ]
+
+// Code 2 : unshift()
+console.log(arr.unshift("test")); // 7
+console.log(arr);
+
+/**[
+  'test', 'one',
+  'two',  'three',
+  'four', 'five',
+  'six'
+] */
+
+// Code 3 : pop()
+console.log(arr.pop()); // six
+console.log(arr); // [ 'test', 'one', 'two', 'three', 'four', 'five' ]
+
+// Code 4 : shift()
+console.log(arr.shift()); // test
+console.log(arr); // [ 'one', 'two', 'three', 'four', 'five' ]
+
+// Code 5 : splice() - mutate original array
+console.log(arr.splice(2)); // [ 'three', 'four', 'five' ]
+console.log(arr); // [ 'one', 'two' ]
+
+// console.log(arr.splice(0)); // [ 'one', 'two' ]
+// console.log(arr); // []
+
+console.log(arr.splice(2, 0, "New", "Another")); // []
+console.log(arr); // [ 'one', 'two', 'New', 'Another' ]
+
+//splice('start index', 'element to removve', 'element to add' , 'element to add')
+// 2 : start index
+// 0 : element to remove
+// "New" : to be add at 2nd positiom
+// "Another" : to be add after New
+
+console.log(arr.splice(arr.length, 0, "New")); // []
+console.log(arr); // [ 'one', 'two', 'New', 'Another', 'New' ]
+
+console.log(arr.splice(0, 0, "New"));
+console.log(arr); // [ 'New', 'one', 'two', 'New', 'Another', 'New' ]
