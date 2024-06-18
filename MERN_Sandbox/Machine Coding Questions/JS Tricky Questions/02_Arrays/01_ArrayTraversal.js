c2c1ff945ea01e33fc9a52b3533683bc54e7bb37
varsha0501 <khatvani.varsha@yahoo.com>
2024-06-18 22:02:01 +0530

    Answers Updated

--- MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/01_ArrayTraversal.js
+++ MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/01_ArrayTraversal.js
@@ -1,9 +1,45 @@
-/**Q.1) What is the difference between for...in and for...of ?
+/**
+Q.1) What is the difference between for...in and for...of ?
+Ans : for in display the index
+      for of refer the value
+
 Q.2) What will be the output of the below code ?
-Q.3) What will be the output of below statements ? */
+Ans : 
+
+let arr = [1, 3, 7, 9] ;
+
+arr.forEach((ele, index)=>{
+    if(index == 2){
+      break;
+    }
+})
+
+We can not use break statement inside the forEach loop as forEach is 
+the callback function. 
+
+alternative solution : 
+
+arr.every((ele, index) => {
+    if (index == 2) {
+        return false;  // Stop iteration
+    }
+    console.log(ele, index);
+    return true;  // Continue iteration
+});
+
+Q.3) What will be the output of below statements ? 
+Ans : 
+
+let arr = [3, 8, 9] ;
+console.log(arr[arr.length]);
+
+*/
 
 // Code 1
 let arr = [1, 2, 3, 4, 5];
+
+console.log(arr[arr.length]); // undefined
+
 let arr1 = ["Hello", 16, true, 3.3];
 console.log(arr);
 console.log(arr1);
@@ -26,6 +62,10 @@ let JSVersion = ["ES6", "ES7", "ES8", "ES9"];
 for (let i = 0; i < JSVersion.length; i++) {
   console.log(JSVersion[i]);
 }
+
+JSVersion.forEach((ele, index) => {
+  console.log(ele, index);
+});
 
 // for in loop - represent index
 for (let elem in JSVersion) {
--- MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/02_ArrayApplications.js
+++ MERN_Sandbox/Machine Coding Questions/JS Tricky Questions/02_Arrays/02_ArrayApplications.js
@@ -1,14 +1,24 @@
-/**Q.1) What is the difference between push() and unshift() method ?
+/**
+Q.1) What is the difference between push() and unshift() method ?
+
 Q.2) What is the difference between pop() and shift() ?
+
 Q.3) How can you insert an element at a given position ?
+Ans : arr.splice(1,0,"New Element")
+
 Q.4) How can you remove  a specific element ?
+Ans : arr.splice(1,2)
+
 Q.5) What does splice() return ?
+Ans : splice() returns deleted items. 
+
 Q.6)  If there is not element removed then what will splice() method return ?
 
 => let arr=["One","Two","Three","Four","Five"];
      console.log(arr.splice(2,0,"New"));      
 
 => Ans : splice() method will return [] array
+
 */
 
 // array.push() method returns the count of array
@@ -38,7 +48,8 @@ console.log(arr.shift()); // test
 console.log(arr); // [ 'one', 'two', 'three', 'four', 'five' ]
 
 // Code 5 : splice() - mutate original array
-console.log(arr.splice(2)); // [ 'three', 'four', 'five' ]
+// returns deleted items
+console.log(arr.splice(2)); // remved from 2nd index [ 'three', 'four', 'five' ]
 console.log(arr); // [ 'one', 'two' ]
 
 // console.log(arr.splice(0)); // [ 'one', 'two' ]
@@ -47,7 +58,7 @@ console.log(arr); // [ 'one', 'two' ]
 console.log(arr.splice(2, 0, "New", "Another")); // []
 console.log(arr); // [ 'one', 'two', 'New', 'Another' ]
 
-//splice('start index', 'element to removve', 'element to add' , 'element to add')
+//splice('start index', 'element to remove', 'element to add' , 'element to add')
 // 2 : start index
 // 0 : element to remove
 // "New" : to be add at 2nd positiom
