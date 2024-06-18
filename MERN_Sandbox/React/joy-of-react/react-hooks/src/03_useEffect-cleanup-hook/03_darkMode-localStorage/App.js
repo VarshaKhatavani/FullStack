d6c0e8e77af0061eb65dda4ebdf1bccafd13484c
varsha0501 <khatvani.varsha@yahoo.com>
2024-06-18 19:14:27 +0530

    #useEffect and return in hooks { initial } | 18-06-2024

---
+++ MERN_Sandbox/React/joy-of-react/react-hooks/src/03_useEffect-cleanup-hook/03_darkMode-localStorage/App.js
@@ -0,0 +1,37 @@
+/*
+Local Storage cheatsheet:
+
+// To save an item:
+window.localStorage.setItem('is-dark-mode', true);
+
+// To retrieve the value:
+JSON.parse(window.localStorage.getItem('is-dark-mode'));
+*/
+
+import React from 'react';
+
+import Toggle from './Toggle';
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  return (
+    <div
+      className="wrapper"
+      style={{
+        // NOTE: This is a just-for-fun mini demo, not a
+        // full-featured Dark Mode implementation!
+        '--color-bg': isDarkMode ? 'black' : 'white',
+        '--color-text': isDarkMode ? 'white' : 'black',
+      }}
+    >
+      <Toggle
+        label="Dark Mode"
+        checked={isDarkMode}
+        handleToggle={setIsDarkMode}
+      />
+    </div>
+  );
+}
+
+export default App;
