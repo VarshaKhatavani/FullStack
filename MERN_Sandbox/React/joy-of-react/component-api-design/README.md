3aa544e94d96ca9345226decb4bd58ade9a7e86a
varsha0501 <khatvani.varsha@yahoo.com>
2024-07-08 14:47:10 +0530

    feat(code implementation) : props delegation

--- MERN_Sandbox/React/joy-of-react/component-api-design/README.md
+++ MERN_Sandbox/React/joy-of-react/component-api-design/README.md
@@ -20,6 +20,18 @@
 
 <h1> Props Delegation</h1>
 
+## Example : Slider Component
+
+<h3>Use of delegation as a property inside the Slider component</h3>
+
+![alt text](public/Images/image4.png)
+
+## Example : Toggle Component
+
+<h3>Use of delegation as a property inside the Toggle component</h3>
+
+![alt text](image.png)
+
 <h1> ForwardRef </h1>
 
 <h1> Polymorphism </h1>
--- MERN_Sandbox/React/joy-of-react/component-api-design/src/App.js
+++ MERN_Sandbox/React/joy-of-react/component-api-design/src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 
 // import BannerApp from "../src/Components/1-banner-component-redesign/BannerApp";
 // import ProductApp from "../src/Components/2-product-component-redesign/ProductApp";
-import ProductInfoApp from "../src/Components/3-product-style-component/ProductInfoApp";
+// import ProductInfoApp from "../src/Components/3-product-style-component/ProductInfoApp";
+
+// **** Props Delegation ****
+
+// import SliderApp from "./Components/4-slider-props-delegation/SliderApp";
+import ToggleApp from "./Components/5-slider-delegation-style/ToggleApp";
 
 function App() {
   return (
@@ -12,7 +17,9 @@ function App() {
       {/* Component API Design */}
       {/* <BannerApp /> */}
       {/* <ProductApp /> */}
-      <ProductInfoApp />
+      {/* <ProductInfoApp /> */}
+      {/* <SliderApp /> */}
+      <ToggleApp />
     </>
   );
 }
