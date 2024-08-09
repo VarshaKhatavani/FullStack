8c9fe40152eee8b404b63e664ddc1c86bf9a8670
varsha0501 <khatvani.varsha@yahoo.com>
2024-08-09 20:04:53 +0530

    perf: Performance improvements | 09-08-2024

--- React/Refresher/src/components/Shimmer.js
+++ React/Refresher/src/components/Shimmer.js
@@ -1,64 +1,18 @@
+const Shimmer = () => {
+  const shimmerCards = Array.from({ length: 8 }, (_, index) => (
+    <div key={index} className="shimmer-card">
+      <div className="shimmer-content">
+        <div className="shimmer-text long"></div>
+        <div className="shimmer-text"></div>
+      </div>
+    </div>
+  ));
 
-const Shimmer = () =>{
-
-    return <div className="shimmer-container flex flex-wrap items-center justify-between gap-3 p-12">
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>
-
-            <div className="shimmer-card">
-                <div className="shimmer-content">
-                    <div className="shimmer-text long"></div>
-                    <div className="shimmer-text"></div>
-                </div>            
-            </div>             
-    </div> 
-
-}
+  return (
+    <div className="shimmer-container flex flex-wrap items-center justify-between gap-3 p-12">
+      {shimmerCards}
+    </div>
+  );
+};
 
 export default Shimmer;
