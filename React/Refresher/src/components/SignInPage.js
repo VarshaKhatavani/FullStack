8272891af47e7d4a8ab40f2f733e4e4361217827
varsha0501 <khatvani.varsha@yahoo.com>
2024-08-29 22:31:20 +0530

    feat(react-hook-form) :  signIn and signout feature added

---
+++ React/Refresher/src/components/SignInPage.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+console.log(useForm);
+const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    const { email, password } = data;
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log("user from local..SignIn...", user);
+    if (user && user.email === email && user.password === password) {
+      setError("");
+      navigate("/");
+      // alert("Sign in successful!");
+      // Redirect to a protected page or home page
+    } else {
+      setError("Invalid email or password");
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen bg-gray-100 items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-3xl font-bold text-center mb-2">Sign In</h2>
+        <div className="flex items-center">
+          <span className="ml-48"> or</span>{" "}
+          <h4 className="ml-2 text-red-500 font-bold">
+            {" "}
+            <Link to={"/signup"}> create new account </Link>
+          </h4>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: "Email is required" })}
+              // value={email}
+              // onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+              placeholder="Enter your email"
+              // required
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+              {...register("password", { required: "Password  is required" })}
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+              placeholder="Enter your password"
+              // required
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+          {error && <p className="text-red-500">{error}</p>}
+          <button
+            type="submit"
+            className="w-full bg-orange-500 text-white py-2 rounded-md text-lg font-semibold hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
