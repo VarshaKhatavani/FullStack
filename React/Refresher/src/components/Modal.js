0d4f6545f5bf1afdae202b315638182b8d8698f7
varsha0501 <khatvani.varsha@yahoo.com>
2024-09-20 21:19:15 +0530

    style : modal added for restaurant before adding to the cart

---
+++ React/Refresher/src/components/Modal.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Modal.css"; // CSS for styling
+
+const Modal = ({ isOpen, onClose, onConfirm }) => {
+  if (!isOpen) return null; // If modal is not open, return null
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <h2>Items already in cart</h2>
+        <p>
+          Your cart contains items from another restaurant. Would you like to
+          reset your cart for adding items from this restaurant?
+        </p>
+        <div className="modal-buttons">
+          <button onClick={onClose} className="modal-button no-button">
+            NO
+          </button>
+          <button onClick={onConfirm} className="modal-button yes-button">
+            YES, START AFRESH
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
