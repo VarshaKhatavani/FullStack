0d7f2429267a234dc88881ee59e6907fd8a17d0c
varsha0501 <khatvani.varsha@yahoo.com>
2024-11-16 23:35:22 +0530

    fix: cors issue & update read me

--- React/Refresher/src/utils/hooks/useRestaurantMenu.js
+++ React/Refresher/src/utils/hooks/useRestaurantMenu.js
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import { MENU_URL } from "../constants";
+import { useRestaurantContext } from "../../utils/context/RestaurantContext.jsx";
 
 const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState(null);
+  const { location } = useRestaurantContext();
+  console.log(location);
 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(MENU_URL + resId);
-    // console.log(MENU_URL + resId);
+    const data = await fetch(
+      MENU_URL(resId, location?.latitude, location?.longitude)
+    );
     const json = await data.json();
     console.log(json);
     setResInfo(json?.data?.cards);
